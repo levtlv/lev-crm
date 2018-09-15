acf8a52d0fe2a70b60df938ac137a116fbcfc84a
Benny Daon <bennydaon@gmail.com>
2018-09-15 17:19:48 +0300

    Fixing hood choices

--- denizen/models.py
+++ denizen/models.py
@@ -19,8 +19,28 @@ VOLUNTEER_CHOICES = (
 )
 
 HOOD_CHOICES = (
-    (None, _('unknown')),
-    ('ON', _('Old North')),
+    ('בבלי צמרות איילון והצפון החדש', 'בבלי צמרות איילון והצפון החדש'),
+    ('הכרם ונווה צדק', 'הכרם ונווה צדק'),
+    ('הצפון הישן (צפוני ודרומי)', 'הצפון הישן (צפוני ודרומי)'),
+    ('התקווה ודרום מזרח העיר', 'התקווה ודרום מזרח העיר'),
+    ('יד אליהו','יד אליהו'),
+    ('יפו','יפו'),
+    ('לב העיר', 'לב העיר'),
+    ('מעוז אביב והדר יוסף','מעוז אביב והדר יוסף'),
+    ("נאות אפקה א' ו-ב'", "נאות אפקה א' ו-ב'"),
+    ('נווה אביבים וסביבתה ורמת אביב','נווה אביבים וסביבתה ורמת אביב'),
+    ('נחלת יצחק בצרון ורמת ישראל','נחלת יצחק בצרון ורמת ישראל'),
+    ("עג'מי וגבעת עלייה","עג'מי וגבעת עלייה"),
+    ('פלורנטין ונווה שאנן','פלורנטין ונווה שאנן'),
+    ('צהלה והמשתלה', 'צהלה והמשתלה'),
+    ('צפון חדש - כיכר המדינה והדרומי', 'צפון חדש - כיכר המדינה והדרומי'),
+    ('צפון יפו', 'צפון יפו'),
+    ('צפון מערביות', 'צפון מערביות'),
+    ('רביבים שיכון דן ונווה דן', 'רביבים שיכון דן ונווה דן'),
+    ("רמת אביב ג' ואפקה", "רמת אביב ג' ואפקה"),
+    ('רמת החייל ונווה שרת', 'רמת החייל ונווה שרת'),
+    ('שפירא קרית שלום וסביבתה', 'שפירא קרית שלום וסביבתה'),
+    ('תל ברוך ותל ברוך צפון', 'תל ברוך ותל ברוך צפון'),
 )
 
 class Denizen(models.Model):
@@ -33,7 +53,7 @@ class Denizen(models.Model):
     state = models.CharField(
         _("state"), blank=True, max_length=1, null=True, choices=STATE_CHOICES)
     hood = models.CharField(_("Neighborhood"),
-        blank=True, max_length=2, choices = HOOD_CHOICES)
+        blank=True, max_length=32, choices=HOOD_CHOICES)
     election_day = models.CharField(_("willing to voounteer on election day"),
         blank=True, max_length=1, choices=VOLUNTEER_CHOICES)
     second_ring = models.CharField(_("willing to be in the second ring"),
@@ -41,9 +61,9 @@ class Denizen(models.Model):
     home_meetup = models.CharField(_("willing to host a meetup"),
                                    blank=True, max_length=1, choices=VOLUNTEER_CHOICES)
     palrig = models.CharField(_("willing to hang a palrig"),
-                              blank=True,max_length=1, choices=VOLUNTEER_CHOICES)
+                              blank=True, max_length=1, choices=VOLUNTEER_CHOICES)
     digital = models.CharField(_("willing to  digital volounteer"),
-                               blank=True,max_length=1, choices=VOLUNTEER_CHOICES)
+                               blank=True, max_length=1, choices=VOLUNTEER_CHOICES)
     women_rights = models.BooleanField(
         _("Interested in women rights"), default=False)
     participation = models.BooleanField(
