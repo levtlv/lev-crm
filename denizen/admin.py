9f3baeb2b25a81d94f9eaae4a5dbe1779911873d
benny daon <bennydaon@gmail.com>
2018-09-22 12:21:40 +0300

    Adding the new fields to the admin

--- denizen/admin.py
+++ denizen/admin.py
@@ -8,16 +8,20 @@ from denizen.models import Denizen
 class DenizenAdmin(admin.ModelAdmin):
     search_fields = ('name', 'source')
     list_filter = ('state', 'election_day', 'second_ring',
-                   'home_meetup', 'palrig', 'digital', 'hood')
+                   'home_meetup', 'meetup_guest', 'palrig', 'digital',
+                   'call_center', 'field', 'hood')
     list_display = ('name', 'phone', 'state', 'hood')
     fieldsets = (
         (None, {
-            'fields': ('name', 'phone', 'email', 'state', 'hood', 'address', 'notes')
+            'fields': ('name', 'phone', 'email', 'state', 'hood', 'address',
+                       'notes', 'edu_foundations')
         }),
         (_('Intrests'), {
             'fields': ('women_rights', 'participation', 'commons', 'education', 'religion'),
         }),
         (_('Volunteering'), {
-            'fields': ('election_day', 'second_ring', 'home_meetup', 'palrig', 'digital'),
+            'fields': ('election_day', 'second_ring', 'home_meetup',
+                       'meetup_guest', 'palrig', 'digital', 'field',
+                       'call_center'),
         }),
     )
